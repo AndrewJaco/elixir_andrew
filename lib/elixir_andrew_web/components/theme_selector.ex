f98d7673dbffcc12391cf9ecf92aa6a7a1c0ae4c
andrewjacobus <andrew@cogini.com>
2025-05-16 17:58:16 +0800

    styling for the theme selector and top dropdown

--- lib/elixir_andrew_web/components/theme_selector.ex
+++ lib/elixir_andrew_web/components/theme_selector.ex
@@ -6,16 +6,15 @@ defmodule ElixirAndrewWeb.Component.ThemeSelector do
   """
   def render(assigns) do
     ~H"""
-    <div class="p-4" id="theme-selector" phx-hook="ThemeHandler" data-theme={@theme}>
-      <form phx-change="change_theme" class="text-secondary mb-4">
-        <label for="theme" class="block mb-2 font-semibold">Choose a Theme:</label>
+    <div class="py-4 px-4" id="theme-selector" phx-hook="ThemeHandler" data-theme={@theme}>
+      <form phx-change="change_theme" class="text-secondary">
         <select 
           id="theme" 
           name="theme" 
           phx-change="change_theme" 
           phx-target={@myself}
-          class="p-2 border rounded">
-          <option value="" selected={@theme == ""}>Teacher Andrew</option>
+          class="p-2 w-full border border-primary rounded-md bg-none text-center appearance-none">
+          <option value="" selected={@theme == ""}>T. Andrew</option>
           <option value="theme-mc" selected={@theme == "theme-mc"}>Minecraft</option>
           <option value="theme-hk" selected={@theme == "theme-hk"}>Hello Kitty</option>
           <option value="theme-lg" selected={@theme == "theme-lg"}>Lego</option>
